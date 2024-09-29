6cdcae654f63f91f33c02629834518befdcd9977
TylerDurham <TylerDurham@users.noreply.github.com>
2024-09-29 16:31:08 -0500

    Updated install/uninstall scripts to reflect new package structure.

--- uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-rm ~/.local/bin/screenshot
-rm -rf ~/.config/sway
-rm -rf ~/.config/swaynag
-rm -rf ~/.config/waybar
-rm -rf ~/.config/rofi
-rm -rf ~/.config/dunst
-rm -rf ~/.local/share/icons
