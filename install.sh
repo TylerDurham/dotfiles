77cb154334ebca6191fc2313de5df8d13d59d605
TylerDurham <TylerDurham@users.noreply.github.com>
2024-09-28 20:00:23 -0500

    Added -stow option. 	modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,23 @@
 #!/bin/sh
 
-cp -r ./.config/** ~/.config/
-cp -r ./.local/bin/* ~/.local/bin/
+OPTSTRING=":s"
+use_stow=0
+
+while getopts ${OPTSTRING} opt; do
+  case $opt in 
+    s)
+      use_stow=1
+      ;;
+
+  esac
+done
+
+if [ $use_stow -eq 0 ]
+then
+  echo "Copying, not stow'ing."
+  #cp -r ./.config/** ~/.config/
+  #11cp -r ./.local/bin/* ~/.local/bin/
+else
+  echo "Stow'ing, not copying."
+fi
+
