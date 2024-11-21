d61c7a3c1073b28cff7e6c9bbde3ea226c046c51
TylerDurham <TylerDurham@users.noreply.github.com>
2024-11-21 17:21:59 -0600

    Random wallpaper effects, code cleanup 	modified:   @hyprland/.config/waybar/scripts/wallpaper.sh

--- @hyprland/.config/waybar/scripts/wallpaper.sh
+++ @hyprland/.config/waybar/scripts/wallpaper.sh
@@ -1,16 +1,30 @@
 #!/usr/bin/bash
 
+IMG_DIR=$HOME/.local/share/wallpapers/
+
 # Transition config (type swww img --help for more settings
 FPS=30
-TYPE="simple"
 DURATION=3
 
-IMG_DIR=$HOME/.local/share/wallpapers/
-PICS=($(ls ${IMG_DIR} | grep -e ".jpg$" -e ".jpeg$" -e ".png$" -e ".gif$"))
+# Random effect
+TRAN_TYPES=("grow" "fade" "simple" "wave" "wipe" "left" "right")
+TYPE=${TRAN_TYPES[ $RANDOM % ${#TRAN_TYPES[@]} ]} #${TRAN_TYPES[1]} #
+echo $TYPE
+
+# Swww params
 SWWW_PARAMS="--transition-fps $FPS --transition-type $TYPE --transition-duration $DURATION"
 
+# Grab a random wallpaper image
+PICS=($(ls ${IMG_DIR} | grep -e ".jpg$" -e ".jpeg$" -e ".png$" -e ".gif$"))
 RANDOM_PIC=${PICS[ $RANDOM % ${#PICS[@]} ]}
-RANDOM_PIC_NAME="${#PICS[@]}. random"
 
-echo $RANDOM_PIC
-swww img $IMG_DIR/$RANDOM_PIC $SWWW_PARAMS
+# Get the extension
+EXT="${RANDOM_PIC##*.}"
+
+# Copy to wallpaper dir, renaming it to "current"
+# Slip the extension in in case we need it later
+NEW_FILE_NAME=$IMG_DIR.current.bak
+NEW_FILE_NAME="${NEW_FILE_NAME/.bak/".$EXT.bak"}"
+cp $IMG_DIR$RANDOM_PIC $NEW_FILE_NAME
+
+swww img $NEW_FILE_NAME $SWWW_PARAMS
