e9fa50135b919a0f96969b2b705e3cecd0756cc8
TylerDurham <TylerDurham@users.noreply.github.com>
2025-05-04 00:44:19 -0500

    LSP for Lua.

--- @nvchad/.config/nvim/lua/plugins/init.lua
+++ @nvchad/.config/nvim/lua/plugins/init.lua
@@ -19,7 +19,9 @@ return {
     -- These are some examples, uncomment them if you want to see them work!
     {
         "neovim/nvim-lspconfig",
+        event = { "BufReadPre", "BufNewFile" },
         config = function()
+            require("nvchad.configs.lspconfig").defaults()
             require("configs.lspconfig")
         end,
     },
