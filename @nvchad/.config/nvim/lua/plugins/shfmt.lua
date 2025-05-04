cdd2a999a1ad5ecd5a89e99f93b164d245b29750
TylerDurham <TylerDurham@users.noreply.github.com>
2025-05-03 23:42:07 -0500

    typo in comment.

--- @nvchad/.config/nvim/lua/plugins/shfmt.lua
+++ @nvchad/.config/nvim/lua/plugins/shfmt.lua
@@ -3,7 +3,7 @@ _  _ ____ ____ ____ ____ ____ _  _ ____ ___
 |\ | |___ |  | |___ |  | |__/ |\/| |__|  |  
 | \| |___ |__| |    |__| |  \ |  | |  |  |  
 
-A simple plugin that downloads shmt (https://github.com/mvdan/sh).
+A simple plugin that downloads shfmt (https://github.com/mvdan/sh).
 
 --]]
 return {
