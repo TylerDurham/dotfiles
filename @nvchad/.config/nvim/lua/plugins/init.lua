ecd171052bf3966d328a77576d500ecf082703f9
TylerDurham <TylerDurham@users.noreply.github.com>
2025-05-04 17:15:01 -0500

    Treesitter Lua enabled.

--- @nvchad/.config/nvim/lua/plugins/init.lua
+++ @nvchad/.config/nvim/lua/plugins/init.lua
@@ -1,37 +1,41 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
-
-{
-  "williamboman/mason.nvim",
-  opts = {
-    ensure_installed = {
-      "lua-language-server",
-      "stylua",
+    {
+        "stevearc/conform.nvim",
+        event = "BufWritePre", -- uncomment for format on save
+        opts = require "configs.conform",
     },
-  },
-},
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
 
-  -- test new blink
-  -- { import = "nvchad.blink.lazyspec" },
+    {
+        "williamboman/mason.nvim",
+        opts = {
+            ensure_installed = {
+                "lua-language-server",
+                "lua_ls",
+                "stylua",
+            },
+        },
+    },
+    -- These are some examples, uncomment them if you want to see them work!
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require "configs.lspconfig"
+        end,
+    },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+    -- test new blink
+    -- { import = "nvchad.blink.lazyspec" },
+
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            ensure_installed = {
+                "vim",
+                "lua",
+                "vimdoc",
+                "html",
+                "css",
+            },
+        },
+    },
 }
