2af32a4fc2b6b8b4554bacd2e29536027bb3d023
TylerDurham <TylerDurham@users.noreply.github.com>
2025-05-06 22:37:56 -0500

    Added ttelescope, whichkey.

---
+++ @nvim-tylerd/.config/nvim/lua/tylerd/plugins/telescope.lua
@@ -0,0 +1,54 @@
+return {
+	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		"nvim-tree/nvim-web-devicons",
+		"andrew-george/telescope-themes",
+	},
+	config = function()
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
+		local builtin = require("telescope.builtin")
+
+		telescope.load_extension("fzf")
+		telescope.load_extension("themes")
+
+		telescope.setup({
+			defaults = {
+				path_display = { "smart" }, -- Shortens file path.
+				mappings = {
+					i = {
+						["<C-k>"] = actions.move_selection_previous,
+						["<C-j>"] = actions.move_selection_next,
+					},
+				},
+			},
+			extensions = {
+				themes = {
+					enable_previewer = true,
+					enable_live_preview = true,
+					persist = {
+						enabled = true,
+						path = vim.fn.stdpath("config") .. "/lua/colorscheme.lua",
+					},
+				},
+			},
+		})
+
+		-- Keymaps
+		vim.keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>", { desc = "Fuzzy find recent files" })
+		vim.keymap.set("n", "<leader>fw", function()
+			local word = vim.fn.expand("<cWORD>")
+			builtin.grep_string({ search = word })
+		end, { desc = "Find Connected Words under cursor" })
+
+		vim.keymap.set(
+			"n",
+			"<leader>th",
+			"<cmd>Telescope themes<CR>",
+			{ noremap = true, silent = true, desc = "Theme Switcher" }
+		)
+	end,
+}
