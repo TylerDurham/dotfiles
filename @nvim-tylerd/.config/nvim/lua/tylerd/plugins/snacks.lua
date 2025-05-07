d07c61610347716e2c01bad3ecc068cac1895bbf
TylerDurham <TylerDurham@users.noreply.github.com>
2025-05-07 01:13:14 -0500

    Added snacks.

---
+++ @nvim-tylerd/.config/nvim/lua/tylerd/plugins/snacks.lua
@@ -0,0 +1,13 @@
+-- https://github.com/folke/snacks.nvim
+return {
+  "folke/snacks.nvim",
+    event = "VimEnter",
+    ---@type snacks.Config
+    opts = {
+      dashboard = {
+        
+
+    }
+  }
+}
+
