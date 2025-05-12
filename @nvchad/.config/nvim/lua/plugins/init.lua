e4b3d804401a5a41a91a3ab98a1307a4b0147b4e
TylerDurham <TylerDurham@users.noreply.github.com>
2025-05-11 20:52:24 -0500

    Added JS, TS lsps.

--- @nvchad/.config/nvim/lua/plugins/init.lua
+++ @nvchad/.config/nvim/lua/plugins/init.lua
@@ -7,19 +7,52 @@ return {
 
     {
         "williamboman/mason.nvim",
-        opts = {
-            ensure_installed = {
-                "lua-language-server",
-                "lua_ls",
-                "stylua",
-                "pyright",
-                "black",
-                "ruff",
-                "isort",
-                "gopls",
-                "goimports",
-            },
+        dependencies = {
+            "williamboman/mason-lspconfig.nvim",
+            "WhoIsSethDaniel/mason-tool-installer.nvim",
         },
+        config = function()
+            local mason = require "mason"
+            local mason_lsp_config = require "mason-lspconfig"
+
+            mason.setup {
+
+                ui = {
+                    icons = {
+                        package_installed = "✓",
+                        package_pending = "➜",
+                        package_uninstalled = "✗",
+                    },
+                },
+            }
+
+            mason_lsp_config.setup {
+                ensure_installed = {
+                    "lua_ls",
+                    "ts_ls",
+                    "pyright",
+                    "gopls",
+                    "html",
+                    "cssls",
+                    "emmet_ls",
+                },
+            }
+
+            local mason_tool_installer = require "mason-tool-installer"
+            mason_tool_installer.setup {
+                ensure_installed = {
+                    "prettier", -- prettier formatter
+                    "stylua", -- lua formatter
+                    "isort", -- python formatter
+                    "black", -- python formatter
+                    "ruff",
+                    "lua-language-server",
+                    "pylint", -- python linter
+                    "eslint_d", -- js linter
+                    "goimports",
+                },
+            }
+        end,
     },
     -- These are some examples, uncomment them if you want to see them work!
     {
