3403c278810c5bc5fafd150a1c363c8d7ed7dfe8
TylerDurham <TylerDurham@users.noreply.github.com>
2025-05-18 00:24:21 -0500

    Now shows errors inline.

--- @nvchad/.config/nvim/lua/options.lua
+++ @nvchad/.config/nvim/lua/options.lua
@@ -1,5 +1,13 @@
 require "nvchad.options"
 
+vim.diagnostic.config {
+    virtual_lines = true,
+    virtual_text = false,
+    severity_sort = true,
+    underline = true,
+    signs = true,
+}
+
 -- add yours here!
 
 -- local o = vim.o
