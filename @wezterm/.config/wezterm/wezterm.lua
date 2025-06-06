cf6e84a4505eca62b78641f53590302616d283e2
TylerDurham <TylerDurham@users.noreply.github.com>
2025-06-06 15:06:13 -0500

    Initial wezterm config 	modified:   @wezterm/.config/wezterm/wezterm.lua

--- @wezterm/.config/wezterm/wezterm.lua
+++ @wezterm/.config/wezterm/wezterm.lua
@@ -1,6 +1,22 @@
+-- Pull in the wezterm API
 local wezterm = require("wezterm")
+
+-- This will hold the configuration.
 local config = wezterm.config_builder()
 
-config.font_size = 16
+-- This is where you actually apply your config choices.
 
+-- For example, changing the initial geometry for new windows:
+config.initial_cols = 120
+config.initial_rows = 28
+
+config.font = wezterm.font("FiraCode Nerd Font Mono")
+
+-- or, changing the font size and color scheme.
+config.font_size = 18
+config.color_scheme = "Catppuccin Mocha"
+
+config.window_background_opacity = 0.85
+
+-- Finally, return the configuration to wezterm:
 return config
