8625ccd79f5066e645fb3d438ad04a1830b4cce3
TylerDurham <TylerDurham@users.noreply.github.com>
2025-07-08 16:38:03 -0500

    Go and Node.

---
+++ .config/zsh/linux.zsh
@@ -0,0 +1,5 @@
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"  # Loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && source "$NVM_DIR/bash_completion"  # Optional: Loads nvm bash_completion
+
+export PATH=$PATH:/usr/local/go/bin
