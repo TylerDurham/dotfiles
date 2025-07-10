a39840f7b413f23a75fc5b9977a240f763195ff3
TylerDurham <TylerDurham@users.noreply.github.com>
2025-07-10 16:48:57 -0500

    MacOs node/nvm exports.

--- .config/zsh/macos.zsh
+++ .config/zsh/macos.zsh
@@ -4,3 +4,8 @@
 # =======================================================================================
 
 export PATH="/opt/homebrew/opt/mysql-client/bin:$PATH"
+
+# NODE STUFF
+export NVM_DIR="$HOME/.nvm"
+  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
