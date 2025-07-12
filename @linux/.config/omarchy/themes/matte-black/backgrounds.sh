1246bc4df9961a66f6fdeb6b6baf689f3d9e280d
TylerDurham <TylerDurham@users.noreply.github.com>
2025-07-12 00:06:18 -0500

    oops. broke themes in omarchy.

--- @linux/.config/omarchy/themes/matte-black/backgrounds.sh
+++
@@ -1,2 +0,0 @@
-mkdir -p "$BACKGROUNDS_DIR/matte-black"
-download_background_image "https://github.com/tahayvr/omarchy-matte-black/blob/main/images/1-matte-black.jpg" "matte-black/1-matte-black.jpg"
