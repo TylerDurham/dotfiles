4cbb9facbb485aec9f6e7239ca7bced7609df383
Tyler Durham <TylerDurham@noreply.users.github.com>
2025-07-14 20:24:35 -0500

    ssh-agent fix in tmux.

--- @core/.config/zsh/linux.zsh
+++ @core/.config/zsh/linux.zsh
@@ -4,8 +4,16 @@ export NVM_DIR="$HOME/.nvm"
 
 export PATH=$PATH:/usr/local/go/bin
 
-if ! pgrep -u "$USER" ssh-agent > /dev/null; then
-    eval "$(ssh-agent -s)"
-    ssh-add -l >/dev/null || ssh-add ~/.ssh/id_ed25519
-fi
+# if ! pgrep -u "$USER" ssh-agent > /dev/null; then
+#     eval "$(ssh-agent -s)"
+#     ssh-add -l >/dev/null || ssh-add ~/.ssh/id_ed25519
+# fi
 
+# Put this in ~/.zshrc
+export SSH_AUTH_SOCK="$HOME/.ssh/ssh_auth_sock"
+
+if ! pgrep -u "$USER" ssh-agent > /dev/null 2>&1 || ! [ -S "$SSH_AUTH_SOCK" ]; then
+    rm -f "$SSH_AUTH_SOCK" # cleanup stale socket
+    eval "$(ssh-agent -a "$SSH_AUTH_SOCK" -s)" > /dev/null
+    ssh-add -l >/dev/null 2>&1 || ssh-add ~/.ssh/id_ed25519
+fi
