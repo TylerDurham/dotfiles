00513965d0dbc50e600935e24acca41c82af607f
Tyler Durham <tylerd@rivendell.snork.co>
2025-07-22 20:07:26 -0500

    feature(macos): Seperated casks from homebrew packages. Attempt to remove casks from Apple quarantine.

--- install.sh
+++ install.sh
@@ -26,15 +26,7 @@ stow-recursive() {
     stow "$dirname" -d "$BASE_DIR" -t ~/
 
   done
-
 }
-
-if command -v brew >/dev/null 2>&1; then
- echo "Homebrew installed. Proceeding."
-else
-  echo "Homebrew is NOT installed. Termininating."
-  exit 127
-fi
 
 # SPECIFIC PLATFORM CONFIGS
 if [[ "$(uname)" == "Darwin" ]]; then
--- scripts/macos-packages.zsh
+++ scripts/macos-packages.zsh
@@ -1,26 +1,52 @@
-SNORK_PACKAGES=("tmux" "neovim" "ripgrep" "zoxide" "fzf" "tree" "starship" "stow" "bat" "alacritty" "nikitabobko/tap/aerospace")
+SNORK_PACKAGES=("tmux" "neovim" "ripgrep" "zoxide" "fzf" "tree" "starship" "stow" "bat")
+SNORK_CASKS=("alacritty" "nikitabobko/tap/aerospace" "font-fira-code-nerd-font")
 
 # Sort and overwrite
 IFS=$'\n' SNORK_PACKAGES=($(sort <<<"${SNORK_PACKAGES[*]}"))
 unset IFS
 
+check_cask() {
+  cask=$1
+
+  if brew list --cask | grep -q "$cask"; then
+    echo -e "\tCask '$cask' is installed. Nothing to do."
+  else
+    echo -e "\tCask '$cask' is not installed. Installing now."
+    brew install --cask "$cask"
+  fi
+}
+
 check_package() {
   package=$1
 
   if brew list --formula | grep -q "$package"; then
-    echo -e "\t$package is installed. Nothing to do."
+    echo -e "\tPackage '$package' is installed. Nothing to do."
   else
-    echo -e "\t$package is not installed. Installing now."
-    brew install "$package"
+
+     echo -e "\tPackage '$package' is not installed. Installing now."
+     brew install "$package"
   fi
 }
 
-for package in "${SNORK_PACKAGES[@]}"; do
-  check_package "$package"
+# No Homebrew? No bueno...
+if command -v brew >/dev/null 2>&1; then
+  echo "Homebrew installed. Proceeding."
+else
+  echo "Homebrew is NOT installed. Termininating."
+  echo "Install Homebrew at https://brew.sh/"
+  exit 127
+fi
+
+# Install casks.
+for cask in "${SNORK_CASKS[@]}"; do 
+  check_cask "$cask"
 done
 
-brew tap homebrew/cask-fonts
-brew install --cask font-fira-code-nerd-font
+# Install packages
+for package in "${SNORK_PACKAGES[@]}"; do
+  check_package "$package" 
+done
 
-
-export SNORK_PACKAGES
+# Unlock from quarantine
+xattr -d com.apple.quarantine /Applications/AeroSpace.app
+xattr -d com.apple.quarantine /Applications/Alacritty.app
