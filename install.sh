ce66868688d1e24489a1e23c35721f3d32a12367
Tyler Durham <tylerd@macos.shared>
2025-07-22 19:37:57 -0500

    fix(install): Check for homebrew install on macOs.

--- install.sh
+++ install.sh
@@ -29,8 +29,12 @@ stow-recursive() {
 
 }
 
-echo "Installing core 'stow' packages."
-stow-recursive ./stow/core/
+if command -v brew >/dev/null 2>&1; then
+ echo "Homebrew installed. Proceeding."
+else
+  echo "Homebrew is NOT installed. Termininating."
+  exit 127
+fi
 
 # SPECIFIC PLATFORM CONFIGS
 if [[ "$(uname)" == "Darwin" ]]; then
@@ -43,3 +47,6 @@ else
   echo "TODO: Linux specific stow packages."
   stow-recursive ./stow/linux/
 fi
+
+echo "Installing core 'stow' packages."
+stow-recursive ./stow/core/
