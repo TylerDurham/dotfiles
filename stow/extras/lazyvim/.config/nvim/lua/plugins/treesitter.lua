5dbb53ca8ac42f5e844d33713e46186352218837
Tyler Durham <TylerDurham@noreply.users.github.com>
2025-08-24 20:06:27 -0500

    fix(lazyvim): Reverted to Omarchy defaults.

--- stow/extras/lazyvim/.config/nvim/lua/plugins/treesitter.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "nvim-treesitter/nvim-treesitter",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = { "python", "lua", "javascript", "markdown", "markdown_inline" },
-      auto_install = true,
-    })
-  end,
-}
