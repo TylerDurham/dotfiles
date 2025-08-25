87eca91461ff9c62649fe68eb3e00493a1238c02
Tyler Durham <TylerDurham@noreply.users.github.com>
2025-08-24 20:39:17 -0500

    feature(lazyvim): Override blink.cmp to use tabs for autocompletion.

---
+++ stow/extras/lazyvim/.config/nvim/lua/plugins/supertab.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "saghen/blink.cmp",
+    opts = function(_, opts)
+      opts.keymap = {
+        preset = "super-tab",
+        ["<Tab>"] = { "select_and_accept" },
+        ["<S-Tab>"] = { "select_prev" },
+      }
+    end,
+  },
+}
