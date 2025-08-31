0e2fac2ce24f11d0a305d97b16f349d2f9150936
TylerDurham <TylerDurham@users.noreply.github.com>
2025-08-31 15:26:51 -0500

    feature(lazyvim): Removed unncessary <cmd> that was causing cursor to drop down.

--- stow/extras/lazyvim/.config/nvim/lua/config/keymaps.lua
+++ stow/extras/lazyvim/.config/nvim/lua/config/keymaps.lua
@@ -2,5 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("i", "jj", "<esc><cr>", { desc = "CUSTOM: My insert mode escape keybind." })
+vim.keymap.set("i", "jj", "<esc>", { desc = "custom: my insert mode escape keybind." })
 vim.keymap.set("n", "<leader>ee", ":Neotree focus<cr>")
