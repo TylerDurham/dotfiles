d25b9a85302dd7e9d3e49080d9aea68f1dec02f5
TylerDurham <TylerDurham@users.noreply.github.com>
2025-09-13 21:28:19 -0500

    fix(bufferline): colors

--- stow/extras/my-neovim/.config/nvim/lua/plugins/bufferline.lua
+++ stow/extras/my-neovim/.config/nvim/lua/plugins/bufferline.lua
@@ -2,10 +2,13 @@ return {
   'akinsho/bufferline.nvim',
   version = "*",
   dependencies = 'nvim-tree/nvim-web-devicons',
+  after = "catppuccin",
   config = function()
     local bufferline = require("bufferline")
 
+
     bufferline.setup({
+      highlights = require("catppuccin.groups.integrations.bufferline").get_theme(),
       options = {
         mode = "buffers",
         diagnostics = "nvim_lsp",
@@ -14,7 +17,7 @@ return {
           delay = 200,
           reveal = { 'close' }
         },
-        separator_style = "slant",
+        -- highlights = require("catppuccin.groups.integrations.bufferline").get_theme(),
         --- count is an integer representing total count of errors
         --- level is a string "error" | "warning"
         --- diagnostics_dict is a dictionary from error level ("error", "warning" or "info")to number of errors for each level.
