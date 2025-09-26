370dcd0ba306849ab1655b68b4a1e2d102e7f365
Tyler Durham <TylerDurham@noreply.users.github.com>
2025-09-25 20:35:49 -0500

    update(tmux, hypr): Adopted from Omarchy.

---
+++ omarchy/scripts/install-apps.sh
@@ -0,0 +1,7 @@
+sudo pacman -S flatpak
+
+flatpak install -y flathub com.github.tchx84.Flatseal
+flatpak install -y flathub com.plexamp.Plexamp    
+sudo flatpak override com.plexamp.Plexamp --socket=wayland
+sudo flatpak override com.plexamp.Plexamp --env=GDK_BACKEND=wayland
+sudo flatpak override com.plexamp.Plexamp --env=ELECTRON_ENABLE_WAYLAND=1
