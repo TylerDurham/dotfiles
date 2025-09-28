32039945b8cb0be9e6e05ae577113dbda38f97f5
Tyler Durham <TylerDurham@noreply.users.github.com>
2025-09-28 17:34:28 -0500

    feat(apps): Added Foliate.

--- omarchy/scripts/install-apps.sh
+++ omarchy/scripts/install-apps.sh
@@ -4,7 +4,8 @@ sudo pacman -Sy lsd stow bat tree ripgrep fzf tmux --noconfirm
 sudo pacman -Sy flatpak --noconfirm
 
 flatpak install -y flathub com.github.tchx84.Flatseal
-flatpak install -y flathub com.plexamp.Plexamp    
+flatpak install -y flathub com.plexamp.Plexamp
+flatpak install -y flathub com.github.johnfactotum.Foliate
 sudo flatpak override com.plexamp.Plexamp --socket=wayland
 sudo flatpak override com.plexamp.Plexamp --env=GDK_BACKEND=wayland
 sudo flatpak override com.plexamp.Plexamp --env=ELECTRON_ENABLE_WAYLAND=1
