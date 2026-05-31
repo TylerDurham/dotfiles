db70bfc9facacaeeca0b26fc0ffd59f4f6640f3b
Tyler Durham <TylerDurham@noreply.users.github.com>
2026-05-31 17:51:38 -0500

    feat(clean): source env and logger libs, remove unused paths.txt

--- install.d/core/clean.sh
+++ install.d/core/clean.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+source "$(git rev-parse --show-toplevel)/install.d/envs.sh"
+
+# Libs
+if ! . "$USER_LIB_DIR/bash/require.sh" logger; then
+  echo "Could not load libraries from '$USER_LIB_DIR'!" >&2
+  exit 1
+fi
+
+# Where is this file located at?
+CWD=$(dirname $(realpath $0))
+
 # Parse args and flags
 while [[ $# -gt 0 ]]; do
   case "$1" in
