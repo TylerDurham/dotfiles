fa15670ae4c2bbf02434fccf9d354fefa633b154
Tyler Durham <TylerDurham@noreply.users.github.com>
2026-05-31 18:01:52 -0500

    feat(envs): add USER_CONFIG_DIR, USER_THEME_DIR, and USER_CURRENT_THEME_DIR exports

--- install.d/envs.sh
+++ install.d/envs.sh
@@ -3,13 +3,15 @@ USER_INSTALL_DIR=$(git rev-parse --show-toplevel)
 
 # [[ -z $USER_LIB_DIR ]] && export USER_LIB_DIR="$USER_INSTALL_DIR/.local/share/dtd/lib/"
 #
-# if [[ -z $USER_BIN_DIR ]]; then 
+# if [[ -z $USER_BIN_DIR ]]; then
 #   export USER_BIN_DIR="$USER_INSTALL_DIR/.local/bin"
 #   export PATH="$USER_BIN_DIR:$PATH"
 # fi
 #
 
 export USER_LIB_DIR="$USER_INSTALL_DIR/.local/share/dtd/lib/"
-
+export USER_CONFIG_DIR="$HOME/.config/dtd"
 export USER_BIN_DIR="$USER_INSTALL_DIR/.local/bin"
+export USER_THEME_DIR="$USER_INSTALL_DIR/themes"
+export USER_CURRENT_THEME_DIR="$USER_CONFIG_DIR/current"
 export PATH="$USER_BIN_DIR:$PATH"
