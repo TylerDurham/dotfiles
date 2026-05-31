96ec409f4701903430a7fcc8e003b4cf5dce67ce
Tyler Durham <TylerDurham@noreply.users.github.com>
2026-05-31 18:07:09 -0500

    refactor(envs): switch USER_INSTALL_DIR refs to PROJECT_ROOT_DIR and relocate theme dir

--- install.d/envs.sh
+++ install.d/envs.sh
@@ -9,9 +9,9 @@ USER_INSTALL_DIR=$(git rev-parse --show-toplevel)
 # fi
 #
 
-export USER_LIB_DIR="$USER_INSTALL_DIR/.local/share/dtd/lib/"
+export USER_LIB_DIR="$PROJECT_ROOT_DIR/.local/share/dtd/lib/"
 export USER_CONFIG_DIR="$HOME/.config/dtd"
-export USER_BIN_DIR="$USER_INSTALL_DIR/.local/bin"
-export USER_THEME_DIR="$USER_INSTALL_DIR/themes"
+export USER_BIN_DIR="$PROJECT_ROOT_DIR/.local/bin"
+export USER_THEME_DIR="$PROJECT_ROOT_DIR/.local/dtd/themes"
 export USER_CURRENT_THEME_DIR="$USER_CONFIG_DIR/current"
 export PATH="$USER_BIN_DIR:$PATH"
