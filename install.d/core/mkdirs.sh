9203960ea0ad52fccee931f5201055a75fea786e
TylerDurham <TylerDurham@noreply.users.github.com>
2026-05-31 20:42:34 -0500

    fix(install, mkdirs): Added 1Pasword. Closes issue #3.

--- install.d/core/mkdirs.sh
+++ install.d/core/mkdirs.sh
@@ -4,30 +4,33 @@ source "$(git rev-parse --show-toplevel)/install.d/envs.sh"
 
 # Libs
 if ! . "$USER_LIB_DIR/bash/require.sh" logger; then
-    echo "Could not load libraries from '$USER_LIB_DIR'!" >&2
-    exit 1
+  echo "Could not load libraries from '$USER_LIB_DIR'!" >&2
+  exit 1
 fi
 
 # Where is this file located at?
 CWD=$(dirname $(realpath $0))
 
-while [[ $# -gt 0 ]]; do 
-  case "$1" in 
-    -r|--remove) REMOVE=1; shift ;;
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+  -r | --remove)
+    REMOVE=1
+    shift
+    ;;
   esac
 done
 
 DIRECTORIES=(
   "$HOME/.local/bin"
   "$HOME/.local/shared/dtd"
-  ~/.config/{dtd,current,hypr,waybar,rofi}
+  ~/.config/{dtd,current,hypr,waybar,rofi,1Pasword}
 )
 
-for DIR in "${DIRECTORIES[@]}"; do 
-  if [[ -z "$REMOVE" ]]; then 
+for DIR in "${DIRECTORIES[@]}"; do
+  if [[ -z "$REMOVE" ]]; then
     info "Creating directory at '$DIR'..."
     mkdir -p "$DIR"
-  else 
+  else
     warn "Removing directory at '$DIR'..."
     rm -rf "$DIR"
   fi
