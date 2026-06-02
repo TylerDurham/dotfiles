b16d2ac0f58c845720d24ab0ef210ccff6e18444
TylerDurham <TylerDurham@noreply.users.github.com>
2026-06-01 22:20:03 -0500

    feat(hypr): add logout script and split autologin into getty/sddm modes
    autologin.sh is now a dispatcher controlled by a MODE variable, delegating
    to autologin-getty.sh or autologin-sddm.sh. Also fixes inverted
    enable/disable logic in sddm.sh and adds terminus-font and vim packages.

---
+++ install.d/hypr/autologin-sddm.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Load environment variables
+source "$(git rev-parse --show-toplevel)/install.d/envs.sh"
+
+# Libs
+if ! . "$USER_LIB_DIR/bash/require.sh" logger; then
+    echo "Could not load libraries from '$USER_LIB_DIR'!" >&2
+    exit 1
+fi
+
+# Parse args and flags
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -r|--remove) REMOVE=1; shift ;;
+  esac
+done
+
+if [[ -n "$REMOVE" ]]; then
+  sudo rm -rf /etc/sddm.conf.d/autologin.conf
+else
+  sudo mkdir -p /etc/sddm.conf.d
+
+cat << EOF | sudo tee /etc/sddm.conf.d/autologin.conf
+[Autologin]
+User=$USER
+Session=hyprland
+EOF
+fi
