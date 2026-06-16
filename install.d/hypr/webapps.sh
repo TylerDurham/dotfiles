e23e0d95bbfaf8b48dde340743ea55361e71718a
TylerDurham <TylerDurham@noreply.users.github.com>
2026-06-15 21:17:23 -0500

    feat(webapps): logos Bible app.

--- install.d/hypr/webapps.sh
+++ install.d/hypr/webapps.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+hypr-install-webapp "Logos" "https://app.logos.com" "https://github.com/TylerDurham/app-icons/blob/master/48x48/Logos.png?raw=true"
 hypr-install-webapp "Gitea" "https://gitea.snork.co" "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/svg/gitea.svg"
 hypr-install-webapp "Github" "https://github.com/TylerDurham" "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/svg/github-light.svg"
 hypr-install-webapp "Claude.ai" "https://claude.ai/new" "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/svg/claude-ai.svg"
