f029eafc55725aaff17c2534b9ecd4e8b589f723
TylerDurham <TylerDurham@noreply.users.github.com>
2026-06-16 20:56:21 -0500

    feat(webapps): add Excalidraw to installed web apps

--- install.d/hypr/webapps.sh
+++ install.d/hypr/webapps.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+hypr-install-webapp "Excalidraw" "https://app.excalidraw.com/w/" "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/svg/excalidraw.svg"
 hypr-install-webapp "Logos" "https://app.logos.com" "https://github.com/TylerDurham/app-icons/blob/master/48x48/Logos.png?raw=true"
 hypr-install-webapp "Gitea" "https://gitea.snork.co" "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/svg/gitea.svg"
 hypr-install-webapp "Github" "https://github.com/TylerDurham" "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/svg/github-light.svg"
